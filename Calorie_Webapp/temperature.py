779af63ee19c4b37357a6678cf63992c9787a115
Coolbylaki <lakisd98@gmail.com>
2022-04-16 22:05:33 +0200

    Temperature clas

--- Calorie_Webapp/temperature.py
+++ Calorie_Webapp/temperature.py
@@ -1,10 +1,17 @@
+import requests
+from selectorlib import Extractor
+
+
 class Temperature:
     """
-
+    Represents temperature extract from timeanddate.com/weather webpage.
     """
     def __init__(self, country, city):
-        self.country = country
-        self.city = city
+        self.country = country.lower()
+        self.city = city.lower()
 
     def get(self):
-        pass
+        request = requests.get(f"https://www.timeanddate.com/weather/{self.country}/{self.city}")
+        content = request.text
+        extractor = Extractor.from_yaml_file("temperature.yaml")
+        return extractor.extract(content)['temp'].replace('\xa0°C', "")
