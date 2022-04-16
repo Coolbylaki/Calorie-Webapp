02c72eec9b3d4bb115ea537ec2e64c29eaba73a7
Coolbylaki <lakisd98@gmail.com>
2022-04-16 22:08:50 +0200

    Calorie class

--- Calorie_Webapp/calorie.py
+++ Calorie_Webapp/calorie.py
@@ -10,4 +10,5 @@ class Calorie:
         self.height = height
 
     def calculate(self):
-        pass
+        bmr = 10 * self.weight + 6.25 * self.height - 5 * self.age + 5 - 10 * self.temperature
+        return bmr
